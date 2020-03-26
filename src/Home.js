f49189e76516f81940bb00bd08141ca79abdc28a
Olga <hijazin.olga@gmail.com>
2020-03-26 11:16:50 -0800

    Guac n roll

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,17 @@ const Home = () => {
   return (
     <div className="header bg ">
       <div className="align-self-center text-center">
-        <h1 className="display-3">Guac-A-Mole</h1>
-        <h3 className="display-5">
-          Make the fastest and the most delicious guacamole!
-        </h3>
+        <h1 className="display-3">
+          <strong>
+            <i>Guac-A-Mole</i>
+          </strong>
+        </h1>
+        <br></br>
+        <h3 className="display-5">Make the fastest and</h3>
+
+        <h3 className="display-5 ">The most delicious guacamole! </h3>
         <Link to="/game">
-          <button type="button" className="btn btn-success">
+          <button type="button" className="btn btn-success mt-3">
             Start Game!!!
           </button>
         </Link>
---
+++ src/Result.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Result = props => {
+  return (
+    <div className="header background">
+      <div className="jumbotron">
+        <h1 className="display-4" style={{ color: "#6CC72A" }}>
+          Holy Guacamole!
+        </h1>
+        <p className="lead"> Your score is {props.score}!!!</p>
+        <hr className="my-4"></hr>
+        <p>Bravocado!</p>
+        <Link to="/game">
+          <button
+            type="button"
+            className="btn  mt-3"
+            style={{ backgroundColor: "#FFC513" }}
+          >
+            Play Again?
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default Result;
